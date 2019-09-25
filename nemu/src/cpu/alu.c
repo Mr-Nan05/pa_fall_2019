12e8206efb7a0c77863265d404499a7253d99fdb
tracer-pa-public <tracer@njuics.org>
2019-09-25 21:40:39 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:40:39 up  8:34,  1 user,  load average: 0.62, 0.40, 0.40 df6811d4a4038fa2f6762279cf36b4bf547e1512

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,441 +1,408 @@
 #include "cpu/cpu.h"
 
-uint32_t data_deal(uint32_t data, size_t data_size)
-{
-	return sign_ext(data & (0xFFFFFFFF >> (32 - data_size)), data_size);
+void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	cpu.eflags.CF=result<src;
 }
 
-uint32_t data_ext(uint32_t data, int n)
-{
-	if (n == 16)
-		return sign_ext(data & 0xFFFF, 16);
-	else if (n == 8)
-		return sign_ext(data & 0xFF, 8);
-}
-
-uint32_t forReturn(uint32_t res, int n, size_t data_size)
-{
-	return res & (0xFFFFFFFF >> (n - data_size));
-}
-
-void set_ZF(uint32_t r, size_t s)
-{
-	r = r & (0xFFFFFFFF >> (32 - s));
-	cpu.eflags.ZF = (r == 0) ? 1 : 0;
-}
-
-void set_SF(uint32_t r, size_t s)
-{
-	r = data_deal(r, s);
-	cpu.eflags.SF = sign(r);
-}
-
-void set_PF(uint32_t r)
-{
-	r &= (0xFFFFFFFF >> 24);
-	int i = 0, j = 0;
-	for (; j <= 7; j++)
+void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size) {
+	switch(data_size)
 	{
-		i += r & 0x1;
-		r >>= 1;
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+			break;
+		default:break;
 	}
-
-	if (i % 2 == 0)
-		cpu.eflags.PF = 1;
-	else
-		cpu.eflags.PF = 0;
-	{
-		/* code */
-	}
-}
-
-void set_CF_add(uint32_t result, uint32_t src, size_t data_size)
-{
-	result = data_deal(result, data_size);
-	src = data_deal(src, data_size);
-	cpu.eflags.CF = result < src;
-}
-
-void set_OF_add(uint32_t r, uint32_t s, uint32_t d, size_t size)
-{
-	switch (size)
-	{
-	case 16:
-		r = data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
-		break;
-	case 8:
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
-		break;
-	default:
-		break;
-	}
-
-	if (sign(s) == sign(d))
-	{
-		if (sign(s) != sign(r))
-			cpu.eflags.OF = 1;
+	if(sign(src)==sign(dest)){
+		if(sign(src)!=sign(result))
+			cpu.eflags.OF=1;
 		else
-			cpu.eflags.OF = 0;
+			cpu.eflags.OF=0;
 	}
-	else
-		cpu.eflags.OF = 0;
+	else{
+		cpu.eflags.OF=0;
+	}
 }
 
-void set_CF_adc(uint32_t r, uint32_t s, uint32_t d, size_t size)
-{
-	r = data_deal(r, size);
-	s = data_deal(s, size);
-	d = data_deal(d, size);
-	if (d)
-		cpu.eflags.CF = r <= s ? 1 : 0;
-	else
-		cpu.eflags.CF = r < s ? 1 : 0;
+void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size) {
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	dest=dest&(0xFFFFFFFF>>(32-data_size));
+	if(dest!=0)
+		cpu.eflags.CF= result<=src;
+	if(dest==0)
+		cpu.eflags.CF= result<src;
 }
 
-/* void set_OF_adc(uint32_t r,uint32_t s,uint32_t d,size_t size)
-{
-	if(size==16)
+void set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+	switch(data_size)
 	{
-		 data_ext(r,16);
-			s = data_ext(s,16);
-			d = data_ext(d,16);
-	}else if(size==8)
-	{
-		r = data_ext(r,8);
-						s= data_ext(s,8);
-						d = data_ext(d,8);
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+			break;
+		default:break;
 	}
-if(sign(s)==sign(d))
-		cpu.eflags.OF=sign(s)!=sign(r)?1:0;
-else  cpu.eflags.OF=0;
-}*/
-
-void set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
-{
-	switch (data_size)
-	{
-	case 8:
-		result = sign_ext(result & 0xFF, 8);
-		src = sign_ext(src & 0xFF, 8);
-		dest = sign_ext(dest & 0xFF, 8);
-		break;
-	case 16:
-		result = sign_ext(result & 0xFFFF, 16);
-		src = sign_ext(src & 0xFFFF, 16);
-		dest = sign_ext(dest & 0xFFFF, 16);
-		break;
-	default:
-		break;
-	}
-	if (sign(src) == sign(dest))
-	{
-		if (sign(src) != sign(result))
-			cpu.eflags.OF = 1;
+	if(sign(src)==sign(dest)){
+		if(sign(src)!=sign(result))
+			cpu.eflags.OF=1;
 		else
-			cpu.eflags.OF = 0;
+			cpu.eflags.OF=0;
 	}
 	else
 	{
-		cpu.eflags.OF = 0;
+		cpu.eflags.OF=0;
 	}
 }
 
-void set_CF_sub(uint32_t r, uint32_t s, size_t data_size)
-{
-	s = data_deal(s, data_size);
-	cpu.eflags.CF = s ? !(r < (~s + 1)) : 0;
+void set_CF_sub(uint32_t result,uint32_t src,size_t data_size){
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	if(src != 0)
+		cpu.eflags.CF=!(result<(~src+1));
+	else
+		cpu.eflags.CF=0;
 }
 
-void set_OF_sub(uint32_t r, uint32_t s, uint32_t d, size_t size)
+void set_OF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
 {
-	if (size == 16)
+	switch(data_size)
 	{
-		r = data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+			break;
+		default:break;
 	}
-	else if (size == 8)
-	{
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
-	}
-	if (sign(~s) == sign(d))
-	{
-		cpu.eflags.OF = (sign(~s) != sign(r));
+	if(sign(~src)==sign(dest)){
+		if(sign(~src)!=sign(result))
+			cpu.eflags.OF=1;
+		else
+			cpu.eflags.OF=0;
 	}
 	else
-		cpu.eflags.OF = 0;
+		cpu.eflags.OF=0;
 }
 
-//point1
-void set_CF_sbb(uint32_t r, uint32_t s, uint32_t d, size_t size)
-{
-	r = data_deal(r, size);
-	s = data_deal(s, size);
-	d = data_deal(d, size);
-	if (s == 0)
-		cpu.eflags.CF = r > d ? 1 : 0;
-	else
-		cpu.eflags.CF = r >= d ? 1 : 0;
-}
-
-void set_OF_sbb(uint32_t r, uint32_t s, uint32_t d, size_t size)
-{
-	if (size == 8)
-	{
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
-	}
-	else if (size == 16)
-	{
-		r = data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
-	}
-	if (sign(~s) == sign(d))
-		cpu.eflags.OF = sign(~s) != sign(r) ? 1 : 0;
-	else
-		cpu.eflags.OF = 0;
-}
-
-void set_OF_CF_mul(uint64_t r, uint32_t d, uint32_t res, size_t size)
-{
-	if (size == 8)
-		cpu.eflags.CF = cpu.eflags.OF = (((r >> 8) | 0x00) > 0);
-	else if (size == 16)
-		cpu.eflags.CF = cpu.eflags.OF = (((r >> 16) | 0x0000) > 0);
-	else if (size == 32)
-		cpu.eflags.CF = cpu.eflags.OF = (((r >> 32) | 0x00000000) > 0);
-}
-
-void set_OF_CF_imul(int64_t r, int32_t d, int32_t s, size_t size)
-{
-	if (size == 32)
-		cpu.eflags.CF = cpu.eflags.OF = !((((r >> 31) ^ 0x1FFFFFFFF) == 0) | (((r >> 31) | 0x000000000) == 0));
-	else if (size == 16)
-		cpu.eflags.CF = cpu.eflags.OF = !((((r >> 15) ^ 0x1FFFF) == 0) | (((r >> 15) | 0x00000) == 0));
-	else if (size == 8)
-		cpu.eflags.CF = cpu.eflags.OF = !((((r >> 7) ^ 0x1FF) == 0) | (((r >> 7) | 0x000) == 0));
-}
-
-uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
-{
-	uint32_t res = 0;
-	res = dest + src;
-
-	set_CF_add(res, src, data_size);
-	set_OF_add(res, src, dest, data_size);
-	set_PF(res);
-	set_SF(res, data_size);
-	set_ZF(res, data_size);
-	return forReturn(res, 32, data_size);
-}
-
-/* uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
-{
-uint32_t res=0;
-	res=dest+src+cpu.eflags.CF;
+void set_CF_sbb(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	dest=sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src=sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
 	
+	if(src!=0)
+		cpu.eflags.CF= (result>=dest);
+	else
+		cpu.eflags.CF= result>dest;
+	
+}
+
+void set_OF_sbb(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+	switch(data_size)
+	{
+		case 8:
+			result=sign_ext(result&0xFF,8);
+			src=sign_ext(src&0xFF,8);
+			dest=sign_ext(dest&0xFF,8);
+			break;
+		case 16:
+			result=sign_ext(result&0xFFFF,16);
+			src=sign_ext(src&0xFFFF,16);
+			dest=sign_ext(dest&0xFFFF,16);
+			break;
+		default:break;
+	}
+	if(sign(~src)==sign(dest)){
+		if(sign(~src)!=sign(result))
+			cpu.eflags.OF=1;
+		else
+			cpu.eflags.OF=0;
+	}
+	else
+		cpu.eflags.OF=0;
+}
+
+void set_OF_CF_mul(uint64_t result,uint32_t dest,uint32_t res,size_t data_size){
+	switch(data_size)
+	{
+		case 8: cpu.eflags.CF=cpu.eflags.OF= (((result>>8)|0x00)>0);
+				break;
+		case 16: cpu.eflags.CF=cpu.eflags.OF= (((result>>16)|0x0000)>0);
+				 break;
+		case 32: cpu.eflags.CF=cpu.eflags.OF= (((result>>32)|0x00000000)>0);
+				 break;
+	}
+}
+
+void set_OF_CF_imul(int64_t result,int32_t dest,int32_t src,size_t data_size){
+	switch(data_size)
+	{
+		case 8: cpu.eflags.CF=cpu.eflags.OF=!((((result>>7)^0x1FF)==0)|(((result>>7)|0x000)==0));
+				break;
+		case 16: cpu.eflags.CF=cpu.eflags.OF=!((((result>>15)^0x1FFFF)==0)|(((result>>15)|0x00000)==0));
+				break;
+		case 32: cpu.eflags.CF=cpu.eflags.OF=!((((result>>31)^0x1FFFFFFFF)==0)|(((result>>31)|0x000000000)==0));
+				break;
+	}
+}
+
+void set_ZF(uint32_t result,size_t data_size) {
+	result=result&(0xFFFFFFFF>>(32-data_size));
+	cpu.eflags.ZF = (result==0);
+}
+
+void set_SF(uint32_t result,size_t data_size) {
+	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	cpu.eflags.SF=sign(result);
+}
+
+void set_PF(uint32_t result)
+{
+	result=result &(0xFFFFFFFF>>24);
+	int temp=0;
+	for(int i=0;i<8;i++)
+	{
+		temp+=result&0x1;
+		result=result>>1;
+	}
+
+	if(temp%2==0)
+		cpu.eflags.PF=1;
+	else
+		cpu.eflags.PF=0;
+}
+ 	
+uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res=dest+src;
+	
+	set_CF_add(res,src,data_size);
+	set_OF_add(res,src,dest,data_size);
+	set_PF(res);
+	set_SF(res,data_size);
+	set_ZF(res,data_size);	
+
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res=dest+src+cpu.eflags.CF;
+
 	set_CF_adc(res,src,dest,data_size);
 	set_OF_adc(res,src,dest,data_size);
 	set_PF(res);
 	set_SF(res,data_size);
-	set_ZF(res,data_size);	
-	return forReturn(res,32,data_size);
-}*/
+	set_ZF(res,data_size);
 
-uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
-{
-	uint32_t res = 0;
-	res = dest + src + cpu.eflags.CF;
-
-	set_CF_adc(res, src, dest, data_size);
-	set_OF_adc(res, src, dest, data_size);
-	set_PF(res);
-	set_SF(res, data_size);
-	set_ZF(res, data_size);
-
-	return res & (0xFFFFFFFF >> (32 - data_size));
+	return res&(0xFFFFFFFF>>(32-data_size));
 }
-	uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		uint32_t res = 0;
-		res = data_deal(dest, data_size) - data_deal(src, data_size);
 
+
+uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res=sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size)-sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+	
+	set_PF(res);
+	set_SF(res,data_size);
+	set_ZF(res,data_size);
+	set_OF_sub(res,src,dest,data_size);
+	set_CF_sub(res,src,data_size);
+
+    return res&(0xFFFFFFFF>>(64-data_size));
+}
+
+uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res=sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size)-(sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size)+cpu.eflags.CF);
+
+	set_PF(res);
+	set_SF(res,data_size);
+	set_ZF(res,data_size);
+	set_OF_sbb(res,src,dest,data_size);
+	set_CF_sbb(res,src,dest,data_size);
+	
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+
+uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
+	uint64_t res=0;
+	res = (uint64_t)dest*src;
+	set_OF_CF_mul(res,dest,src,data_size);
+	return res;
+}
+
+int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
+	int64_t res=0;
+	res=(int64_t)dest*src;
+	set_OF_CF_imul(res,dest,src,data_size);
+	return res;
+}
+
+uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size) {
+	uint32_t res=0;
+	res=dest/src;
+	return res;
+}
+
+int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size) {
+	int32_t res=0;
+	res=dest/src;
+	return res;
+}
+
+uint32_t alu_mod(uint64_t src, uint64_t dest) {
+	return dest%src;
+}
+
+int32_t alu_imod(int64_t src, int64_t dest) {
+	return dest%src;
+}
+
+uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res = dest & src;
+
+	cpu.eflags.CF=0;
+	cpu.eflags.OF=0;
+	set_PF(res);
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+uint32_t alu_xor(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res = dest^src;
+
+	cpu.eflags.CF=0;
+	cpu.eflags.OF=0;
+	set_PF(res);
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	res = dest | src;
+
+	cpu.eflags.CF=0;
+	cpu.eflags.OF=0;
+	set_PF(res);
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
+
+	uint32_t res=0;
+	if(src==0)
+		return dest;
+	dest=dest&(0xFFFFFFFF>>(32-data_size));
+	while(src--)
+	{
+		cpu.eflags.CF= ((dest&(0x80000000>>(32-data_size)))>0);
+		dest*=2;
+		res=dest;
 		set_PF(res);
-		set_SF(res, data_size);
-		set_ZF(res, data_size);
-		set_OF_sub(res, src, dest, data_size);
-		set_CF_sub(res, src, data_size);
-		return forReturn(res, 64, data_size);
+		set_SF(res,data_size);
+		set_ZF(res,data_size);
 	}
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
 
-	uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
+uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	dest=dest&(0xFFFFFFFF>>(32-data_size));
+	while(src--)
 	{
-		uint32_t res = 0;
-		res = data_deal(dest, data_size) - data_deal(src, data_size + cpu.eflags.CF);
+		cpu.eflags.CF= ((dest&0x00000001)>0);
+		dest/=2;
+		res=dest;
 		set_PF(res);
-		set_SF(res, data_size);
-		set_ZF(res, data_size);
-		set_OF_sbb(res, src, dest, data_size);
-		set_CF_sbb(res, src, dest, data_size);
-
-		return forReturn(res, 32, data_size);
+		set_SF(res,data_size);
+		set_ZF(res,data_size);
 	}
+	return res&(0xFFFFFFFF>>(32-data_size)); 
+}
 
-	uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
+
+uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	
+	if(src==0)
+		return dest;
+	dest=sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
+	while(src--)
 	{
-		uint64_t res = 0;
-		res = (uint64_t)dest * src;
-		set_OF_CF_mul(res, dest, src, data_size);
-		return (uint64_t)dest * src;
-	}
-
-	int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
-	{
-		int64_t res = 0;
-		res = (int64_t)dest * src;
-		set_OF_CF_imul(res, dest, src, data_size);
-		return (uint64_t)dest * src;
-	}
-
-	// need to implement alu_mod before testing
-	uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size)
-	{
-		return (uint32_t)(dest / src);
-	}
-
-	// need to implement alu_imod before testing
-	int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size)
-	{
-		return (uint32_t)(dest / src);
-	}
-
-	uint32_t alu_mod(uint64_t src, uint64_t dest)
-	{
-		return dest % src;
-	}
-
-	int32_t alu_imod(int64_t src, int64_t dest)
-	{
-		return dest % src;
-	}
-
-	uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		cpu.eflags.CF = 0;
-		cpu.eflags.OF = 0;
-		set_PF(dest & src);
-		set_ZF(dest & src, data_size);
-		set_SF(dest & src, data_size);
-		return forReturn(dest & src, 32, data_size);
-	}
-
-	uint32_t alu_xor(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		cpu.eflags.CF = 0;
-		cpu.eflags.OF = 0;
-		set_PF(dest ^ src);
-		set_ZF(dest ^ src, data_size);
-		set_SF(dest ^ src, data_size);
-		return forReturn(dest ^ src, 32, data_size);
-	}
-
-	uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		cpu.eflags.CF = 0;
-		cpu.eflags.OF = 0;
-		set_PF(dest | src);
-		set_ZF(dest | src, data_size);
-		set_SF(dest | src, data_size);
-		return forReturn(dest | src, 32, data_size);
-	}
-
-	uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		if (!src)
-			return dest;
-		dest = data_deal(dest, data_size);
-		while (src--)
-		{
-			cpu.eflags.CF = ((dest & (0x80000000 >> (32 - data_size))) > 0);
-			dest *= 2;
-			set_PF(dest);
-			set_SF(dest, data_size);
-			set_ZF(dest, data_size);
-		}
-		return forReturn(dest, 32, data_size);
-	}
-
-	uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
-	{
-
-		dest = data_deal(dest, data_size);
-		while (src--)
-		{
-			cpu.eflags.CF = ((dest & 0x00000001) > 0);
-			dest /= 2;
-			set_PF(dest);
-			set_SF(dest, data_size);
-			set_ZF(dest, data_size);
-		}
-		return forReturn(dest, 32, data_size);
-	}
-
-	uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
-	{
-
-		if (!src)
-			return dest;
-		dest = data_deal(dest, data_size);
-		while (src--)
-		{
-			cpu.eflags.CF = ((dest & 0x00000001) > 0);
-			if (sign(dest) == 1)
-				dest = (0x80000000 >> (32 - data_size)) | (dest / 2);
-			else
-				dest /= 2;
-			set_PF(dest);
-			set_SF(dest, data_size);
-			set_ZF(dest, data_size);
-		}
-		return forReturn(dest, 32, data_size);
-	}
-
-	uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
-	{
-		dest = forReturn(dest, 32, data_size);
-		while (src--)
-		{
-			cpu.eflags.CF = ((dest & (0x80000000 >> (32 - data_size))) > 0);
-			dest *= 2;
-			set_PF(dest);
-			set_SF(dest, data_size);
-			set_ZF(dest, data_size);
-		}
-		return forReturn(dest, 32, data_size);
-	}
-	uint32_t alu_neg(uint32_t src, size_t data_size)
-	{
-
-		uint32_t res = 0;
-		if (src == 0)
-			cpu.eflags.CF = 0;
+		cpu.eflags.CF= ((dest&0x00000001)>0);
+		if(sign(dest)==1)
+			dest = (0x80000000>>(32-data_size))|(dest/2);
 		else
-			cpu.eflags.CF = 1;
-
-		res = ~src + 1;
-
-		set_OF_add(res, (~src), 1, data_size);
+			dest=dest/2;
+		res=dest;
 		set_PF(res);
-		set_SF(res, data_size);
-		set_ZF(res, data_size);
-
-		return forReturn(res, 32, data_size);
+		set_SF(res,data_size);
+		set_ZF(res,data_size);
 	}
+
+	return res&(0xFFFFFFFF>>(32-data_size)); 
+}
+
+uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size) {
+	uint32_t res=0;
+	dest=dest&(0xFFFFFFFF>>(32-data_size));
+	while(src--)
+	{
+		cpu.eflags.CF= ((dest&(0x80000000>>(32-data_size)))>0);
+		dest*=2;
+		res=dest;
+		set_PF(res);
+		set_SF(res,data_size);
+		set_ZF(res,data_size);
+	}
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
+uint32_t alu_neg(uint32_t src,size_t data_size){
+	
+	uint32_t res=0;
+	if(src==0)
+		cpu.eflags.CF=0;
+	else
+		cpu.eflags.CF=1;
+
+	res=~src+1;
+
+	set_OF_add(res,(~src),1,data_size);
+	set_PF(res);
+	set_SF(res,data_size);
+	set_ZF(res,data_size);
+
+	return res&(0xFFFFFFFF>>(32-data_size));
+}
+
