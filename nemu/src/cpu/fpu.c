e58b866fe545f030997a9f3a67943a6035e37e72
monitor <tracer@njuics.org>
2019-09-26 08:42:01 +0800

    git monitor commit.

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -30,7 +30,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		if (exp >= 0xff)
 		{
 			/* TODO: assign the number to infinity */
-			printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
+			
 			exp = 0xff;
 			sig_grs = 0;
 			overflow = true;
