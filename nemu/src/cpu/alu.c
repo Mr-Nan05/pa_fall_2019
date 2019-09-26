de0ba8329790549ba689d0d9ca27d03bfdc16ca3
tracer-pa-public <tracer@njuics.org>
2019-09-26 09:18:48 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  09:18:48 up 25 min,  1 user,  load average: 1.62, 1.14, 0.99 96a13436a0df5dbb408025bdb995ab2b4ad9270a

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -262,7 +262,7 @@ uint32_t res=0;
 	set_ZF(res, data_size);
 
 	return res & (0xFFFFFFFF >> (32 - data_size));
-}
+}*/
 	uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	{
 		uint32_t res = 0;
@@ -274,7 +274,7 @@ uint32_t res=0;
 		set_OF_sub(res, src, dest, data_size);
 		set_CF_sub(res, src, data_size);
 		return forReturn(res, 64, data_size);
-	}*/
+	}
 
 	uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	{
