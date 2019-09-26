b16da325a0e15da7bb47053c5ea2d604c3ab5576
monitor <tracer@njuics.org>
2019-09-26 09:36:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -223,7 +223,7 @@ void set_OF_CF_imul(int64_t r, int32_t d, int32_t s, size_t size)
 		cpu.eflags.CF = cpu.eflags.OF = !((((r >> 7) ^ 0x1FF) == 0) | (((r >> 7) | 0x000) == 0));
 }
 
-uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
+/* uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 	uint32_t res = 0;
 	res = dest + src;
@@ -234,7 +234,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	set_SF(res, data_size);
 	set_ZF(res, data_size);
 	return forReturn(res, 32, data_size);
-}
+}*/
 
  uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
