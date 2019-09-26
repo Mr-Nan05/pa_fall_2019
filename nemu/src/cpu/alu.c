210ef6f45c38e6c14af3904282b7034014132c1a
tracer-pa-public <tracer@njuics.org>
2019-09-26 09:43:39 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  09:43:39 up 50 min,  1 user,  load average: 1.29, 1.74, 1.65 7b3c78a3e54cdcc1349fc81fc2c0d3313972b49

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -124,17 +124,17 @@ void set_CF_adc(uint32_t r, uint32_t s, uint32_t d, size_t size)
 
 void set_OF_adc(uint32_t r, uint32_t s, uint32_t d, size_t size)
 {
-	if (size == 16)
+	if (size == 8)
 	{
-		data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
+		r=sign_ext(r&0xFF,8);
+			s=sign_ext(s&0xFF,8);
+			d=sign_ext(d&0xFF,8);
 	}
-	else if (size == 8)
+	else if (size == 16)
 	{
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
+		r=sign_ext(r&0xFFFF,16);
+			s=sign_ext(s&0xFFFF,16);
+			d=sign_ext(d&0xFFFF,16);
 	}
 	if (sign(s) == sign(d))
 		cpu.eflags.OF = sign(s) != sign(r) ? 1 : 0;
@@ -180,17 +180,17 @@ void set_CF_sub(uint32_t r, uint32_t s, size_t data_size)
 
 void set_OF_sub(uint32_t r, uint32_t s, uint32_t d, size_t size)
 {
-	if (size == 16)
+	if (size == 8)
 	{
-		r = data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
+		r=sign_ext(r&0xFF,8);
+			s=sign_ext(s&0xFF,8);
+			d=sign_ext(d&0xFF,8);
 	}
-	else if (size == 8)
+	else if (size == 16)
 	{
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
+		r=sign_ext(r&0xFFFF,16);
+			s=sign_ext(s&0xFFFF,16);
+			d=sign_ext(d&0xFFFF,16);
 	}
 	if (sign(~s) == sign(d))
 	{
@@ -216,15 +216,15 @@ void set_OF_sbb(uint32_t r, uint32_t s, uint32_t d, size_t size)
 {
 	if (size == 8)
 	{
-		r = data_ext(r, 8);
-		s = data_ext(s, 8);
-		d = data_ext(d, 8);
+		r=sign_ext(r&0xFF,8);
+			s=sign_ext(s&0xFF,8);
+			d=sign_ext(d&0xFF,8);
 	}
 	else if (size == 16)
 	{
-		r = data_ext(r, 16);
-		s = data_ext(s, 16);
-		d = data_ext(d, 16);
+		r=sign_ext(r&0xFFFF,16);
+			s=sign_ext(s&0xFFFF,16);
+			d=sign_ext(d&0xFFFF,16);
 	}
 	if (sign(~s) == sign(d))
 		cpu.eflags.OF = sign(~s) != sign(r) ? 1 : 0;
