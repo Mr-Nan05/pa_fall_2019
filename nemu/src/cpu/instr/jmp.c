c265c9285641a4aebce696887de2596dfa86c163
tracer-pa-public <tracer@njuics.org>
2019-10-28 22:47:09 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:47:09 up  9:13,  1 user,  load average: 0.84, 0.79, 0.78 d11e2ed9885fb49c8476fda06adc74e9d20f3074

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -1,20 +1,76 @@
 #include "cpu/instr.h"
 
-make_instr_func(jmp_near)
-{
+make_instr_func(jmp_near) {
         OPERAND rel;
         rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
+	rel.sreg = SREG_CS;
         rel.data_size = data_size;
         rel.addr = eip + 1;
 
         operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+	int offset = sign_ext(rel.val, data_size);
+	print_asm_1("jmp", "", 2, &rel);
 
-        cpu.eip += offset;
+	cpu.eip += offset;
 
         return 1 + data_size / 8;
+}
+
+make_instr_func(jmp_short_){
+        OPERAND rel;
+        rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+        rel.data_size = 8;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+
+        int offset = sign_ext(rel.val, rel.data_size);
+
+        print_asm_1("jmp", "", 2, &rel);
+
+        cpu.eip += offset;
+        return 1 + rel.data_size / 8;
+}
+
+make_instr_func(jmp_near_indirect){
+        int len = 1;
+        opr_src.data_size = data_size;
+        len += modrm_rm(eip+1,&opr_src);
+        operand_read(&opr_src);
+
+        print_asm_1("jmp", "", 2, &opr_src);
+
+        cpu.eip = opr_src.val;
+
+        return 0;
+}
+
+
+make_instr_func(jmp_far_imm){        
+#ifdef IA32_SEG    
+        int len = 1;
+        OPERAND ptr1, ptr2;
+        ptr1.data_size = 16;
+        ptr2.data_size = data_size;
+        ptr1.type = ptr2.type = OPR_IMM;
+        ptr1.sreg = ptr2.sreg = SREG_CS;
+        ptr2.addr = eip + 1;
+        ptr1.addr = eip + 1 + data_size/8;
+
+        print_asm_2("ljmp","",2,&ptr1,&ptr2);
+
+        operand_read(&ptr1);
+        operand_read(&ptr2);
+        len += 2 + data_size/8;
+        
+        cpu.eip = ptr2.val;
+        cpu.segReg[SREG_CS].val = ptr1.val;
+        load_sreg(SREG_CS);
+
+        return 0;
+#else
+        printf("please implement ljmp\n");assert(0);
+#endif
 }
