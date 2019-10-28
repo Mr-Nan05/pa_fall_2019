5d1c2e6fb2a2780f91c552b8f5a6afeb2b7f085d
tracer-pa-public <tracer@njuics.org>
2019-10-28 22:21:45 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:21:45 up  8:48,  1 user,  load average: 0.50, 0.73, 0.76 20ad36cda9892b29411c10d0a054640c53ce868e

---
+++ nemu/src/cpu/instr/and.c
@@ -0,0 +1,26 @@
+#include "cpu/instr.h"
+#include "cpu/alu.h"
+
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+
+    if(opr_src.data_size==8 && opr_dest.data_size== data_size)
+    {
+        opr_src.val = sign_ext(opr_src.val,8);
+    }
+    opr_dest.val=alu_and(opr_src.val,opr_dest.val,opr_dest.data_size);
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(and, r, rm, b)
+make_instr_impl_2op(and, r, rm, v)
+make_instr_impl_2op(and, rm, r, b)
+make_instr_impl_2op(and, rm, r, v)
+make_instr_impl_2op(and, i, a, b)
+make_instr_impl_2op(and, i, a, v)
+
+make_instr_impl_2op(and,i,rm,b)
+make_instr_impl_2op(and,i,rm,v)
+make_instr_impl_2op(and,i,rm,bv)
