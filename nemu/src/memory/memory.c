13a8919616617a67217d8aeaea87d30ba283a484
tracer-pa-public <tracer@njuics.org>
2019-11-26 16:35:46 +0800

    >  clean 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:35:46 up  4:03,  1 user,  load average: 0.84, 0.70, 0.54 647ba8fd92dc2e724e78c7ea40e471599f6255f0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -23,7 +23,7 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 uint32_t paddr_read(paddr_t paddr, size_t len) {
 uint32_t ret = 0;
 #ifdef CACHE_ENABLED
-ret = cache_read(paddr, len, L1_dcache);
+ret = cache_read(paddr, len, &L1_dcache);
 #else
 ret = hw_mem_read(paddr, len);
 #endif
