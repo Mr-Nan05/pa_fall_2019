fd2b041e84ccdbc3a74083ce033662e2ee11b987
monitor <tracer@njuics.org>
2019-11-26 14:39:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,3 +4,14 @@
 #include "memory/cache.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+CacheLine L1_dcache[line];
+
+void init_cache(){
+    for(int i=0;i<line;i++){
+        L1_dcache[i].flag.validBit=0;
+        L1_dcache[i].flag.tag=0;
+        for(int j=0;j<64;j++)
+            L1_dcache[i].data[j]=0;
+    }
+}
