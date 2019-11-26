d66ea0268394109971433368c84992faab30843b
monitor <tracer@njuics.org>
2019-11-26 14:25:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -20,4 +20,12 @@ typedef struct CacheLine{
 }CacheLine;
 
 
+extern CacheLine L1_dcache[];
+
+void init_cache();
+
+uint32_t cache_read(paddr_t paddr,size_t len,CacheLine *cache);
+
+void cache_write(paddr_t paddr,size_t len,uint32_t data,CacheLine *cache);
+
 
