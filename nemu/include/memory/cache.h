b43b0a800c790a7945d7f8e072ba6039fc801673
monitor <tracer@njuics.org>
2019-11-26 14:26:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,6 +1,5 @@
-
+#ifndef __CACHE_H__
 #define __CACHE_H__
-
 #include "nemu.h"
 #include "memory/mmu/segment.h"
 #include "memory/mmu/page.h"
@@ -28,4 +27,4 @@ uint32_t cache_read(paddr_t paddr,size_t len,CacheLine *cache);
 
 void cache_write(paddr_t paddr,size_t len,uint32_t data,CacheLine *cache);
 
-
+#endif
