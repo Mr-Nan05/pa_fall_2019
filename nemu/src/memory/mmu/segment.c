44cf74c584fee7c10faf270f5eec7cbb99f3ca6b
monitor <tracer@njuics.org>
2019-11-28 09:06:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -7,6 +7,8 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
+
+	
 	return 0;
 }
 
