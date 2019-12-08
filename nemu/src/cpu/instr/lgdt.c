a5501c298168e87b2e10da221de9e20933e9e864
tracer-pa-public <tracer@njuics.org>
2019-12-08 10:22:39 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  10:22:39 up  1:15,  2 users,  load average: 0.16, 0.22, 0.37 2ae439498bc81bb11a6f45626c495cb29fe3af44

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,10 +2,7 @@
 
 void operand_lgdt(OPERAND &limit, OPERAND &base, uint32_t addr)
 {
-    limit.data_size = 16;   base.data_size = 32;
-    limit.type = OPR_MEM;   base.type = OPR_MEM;
-    limit.sreg = SREG_DS;   base.sreg = SREG_DS;
-    limit.addr = addr;      base.addr = addr + 2;
+
 }
 
 //void gdtr_change(OPERAND &base,OPERAND &limit, uint32_t addr)
@@ -21,7 +18,11 @@ make_instr_func(lgdt){
     uint32_t addr = opr_src.addr;
     OPERAND limit,base;
     //gdtr_change(base,limit, addr);
-      operand_lgdt(limit,base, addr);
+      //operand_lgdt(limit,base, addr);
+          limit.data_size = 16;   base.data_size = 32;
+    limit.type = OPR_MEM;   base.type = OPR_MEM;
+    limit.sreg = SREG_DS;   base.sreg = SREG_DS;
+    limit.addr = addr;      base.addr = addr + 2;
     operand_read(&limit);   operand_read(&base);
     cpu.gdtr.limit = limit.val;
     if(data_size == 16)
