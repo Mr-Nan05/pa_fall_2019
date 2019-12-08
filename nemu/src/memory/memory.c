a6a5b0be55e40cae6827c0d30045039ed246eb7e
monitor <tracer@njuics.org>
2019-12-08 09:51:01 +0800

    git monitor commit.

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -101,9 +101,9 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 {
-	assert(len == 1 || len == 2 || len == 4);
-	if(cpu.cr0.pg && cpu.cr0.pe){
-		if((laddr & 0xfff) + len -1 >=0x1000){
+	laddr_assert(len);
+	if(laddr_if1()){
+		if((laddr_if(laddr, len)){
 			size_t len_new = len + (laddr & 0xfff) - 0x1000;
 			uint32_t laddr_new = laddr + (len - len_new);
 			paddr_t paddr = page_translate(laddr);
