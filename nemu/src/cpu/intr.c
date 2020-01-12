18b10a2e26f628cd535d8afcae4ced175bb7ab3b
tracer-pa-public <tracer@njuics.org>
2020-01-12 15:49:16 +0800

    >  nemu 171860642 scq Linux debian32 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  15:49:16 up  1:47,  1 user,  load average: 0.28, 0.53, 0.60 ac047c313e3e8ccdc24ef18fdd3bfff168cf9e

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -12,6 +12,20 @@ void cpu_write()
 	vaddr_write(cpu.esp,SREG_DS,4,cpu.eip);
 }
 
+uint32_t addr_next()
+{
+	GateDesc *addr=(GateDesc *)(hw_mem+phy_base);
+	GateDesc *addr_2=&*(addr+intr_no);
+	if(addr_2->type==0xE) cpu.eflags.IF=0;
+
+	uint32_t offset_1=addr_2->offset_15_0;
+	uint32_t offset_2=addr_2->offset_31_16<<16;
+	uint32_t next=offset_1 | offset_2;
+
+	assert(addr_2->present==1);
+	return next;
+}
+
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
@@ -19,6 +33,12 @@ void raise_intr(uint8_t intr_no)
 	//assert(0);
 
 	cpu_write();
+	uint32_t phy_base=0;
+	if(cpu.cr0.pg==1) phy_base=page_translate(cpu.idtr.base);
+	else phy_base=cpu.idtr.base;
+
+
+	cpu.eip=addr_next();
 
 #endif
 }
